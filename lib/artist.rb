28f84d847551fa81f4088c1fda33f3653d8773fa
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-28 23:10:05 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,47 @@
+class Artist
+  extend Concerns::Findable
+
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    artist = new(name)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    song.artist = self unless song.artist
+    songs << song unless songs.include?(song)
+  end
+
+  def genres
+    songs.collect{ |s| s.genre }.uniq
+  end
+
+  def if_kanye
+    if artist == "#{}"
+      return "Who the hell listens to Kanye anymore? Listen to #48 by Panda Bear."
+    else
+      return song
+  end
+end
